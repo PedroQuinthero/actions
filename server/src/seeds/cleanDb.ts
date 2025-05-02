701c7e92b863f1153a3ab9ee25618d5e66abca8c
Pedro Quintero <pedroquinthero@gmail.com>
2025-05-02 11:42:27 -0700

    updated seeds, trying to render

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -3,14 +3,18 @@ import db from '../config/connection.js';
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
-    let modelExists = await models[modelName].db.db.listCollections({
-      name: collectionName
-    }).toArray()
+    const model = models[modelName];
 
-    if (modelExists.length) {
+    if (!model || !model.db?.db) {
+      throw new Error(`Database connection not found for model ${modelName}`);
+    }
+
+    const collections = await model.db.db.listCollections({ name: collectionName }).toArray();
+
+    if (collections.length) {
       await db.dropCollection(collectionName);
     }
   } catch (err) {
     throw err;
   }
-}
+};
